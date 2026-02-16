097921fceed20a3786ac77b573d6b05195bcf0f2
b1r-dost <178371489+b1r-dost@users.noreply.github.com>
2026-02-16 10:33:07 +0300

    Delete DignoseThatAPISystem directory

--- DignoseThatAPISystem/Console/supabase/migrations/20260122091854_fc3ca3db-e2e0-4d33-b76f-844b0e2dbc8f.sql
+++
@@ -1,4 +0,0 @@
--- Assign admin role to existing user
-INSERT INTO public.user_roles (user_id, role)
-VALUES ('b9ee8998-e81f-43c2-a708-33ec0f30af2f', 'admin')
-ON CONFLICT (user_id, role) DO NOTHING;
